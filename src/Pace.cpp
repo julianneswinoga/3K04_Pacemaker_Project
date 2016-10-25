fb59154511d38bf11629e5a11b0f43532b5febe4
Cameron Swinoga <swinogca@mcmaster.ca>
2016-10-25 17:08:52 -0400

    Added Pace

---
+++ src/Pace.cpp
@@ -0,0 +1,47 @@
+#include "Pace.h"
+
+Pace::Pace()  {
+	
+}
+
+// Public functions
+uint16_t Pace::getHysteresisInterval() {
+	
+}
+
+uint16_t Pace::getLowRateInterval() {
+	
+}
+
+uint16_t Pace::getvPaceAmp() {
+	
+}
+
+uint16_t Pace::getvPaceWidth() {
+	
+}
+
+uint16_t Pace::getVRP() {
+	
+}
+
+// Protected functions
+void Pace::setHysteresisInterval(uint16_t p) {
+	
+}
+
+void Pace::setLowRateInterval(uint16_t p) {
+	
+}
+
+void Pace::setvPaceAmp(uint16_t p) {
+	
+}
+
+void Pace::setvPaceWidth(uint16_t p) {
+	
+}
+
+void Pace::setVRP(uint16_t p) {
+	
+}
