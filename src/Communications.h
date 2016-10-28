f739ee910d32acc34e3d8c568f189bd8885d51d6
laruepa <laruepa@mcmaster.ca>
2016-10-27 23:44:02 -0400

    UML Changes, added variables & methods from UMLs to appropriate class files. Tested, compiles ok.

--- src/Communications.h
+++ src/Communications.h
@@ -13,6 +13,7 @@ class Communications : public Pacemaker {
 		//i_vs:??
 		uint8_t o_CommOut;
 		//o_vp:??
+		int baudRate;
 		
 	public:
 		Communications();
--- src/Pacemaker.cpp
+++ src/Pacemaker.cpp
@@ -3,3 +3,36 @@
 Pacemaker::Pacemaker()  {
 	
 }
+
+//Protected Functions
+void Pacemaker::setMaxVOut(uint16_t) {
+
+}
+
+uint16_t getMaxVOut(){
+	return 1;
+}
+
+void setComPort(uint8_t){
+
+}
+
+uint8_t getComPort(){
+	return 1;
+}
+
+float voltageTest(float){
+	return 1.0;
+}
+
+void clearCardiacEvents(){
+
+}
+
+void setLeadImpedance(float){
+
+}
+
+float getLeadImpedance(){
+	return 1.0;
+}
