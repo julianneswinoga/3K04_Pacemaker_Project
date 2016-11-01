9edc593a725beb201cb030a2102cc8a7d1e7775a
Cameron Swinoga <swinogca@mcmaster.ca>
2016-11-01 14:41:41 -0400

    Minor gui updates

--- gui/pacemakerInterface.py
+++ gui/pacemakerInterface.py
@@ -52,69 +52,128 @@ class MainFrame ( wx.Frame ):
 		
 		gSizer5 = wx.GridSizer( 4, 4, 0, 0 )
 		
-		self.m_staticText4 = wx.StaticText( self, wx.ID_ANY, u"MyLabel", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.m_staticText4.Wrap( -1 )
-		gSizer5.Add( self.m_staticText4, 0, wx.ALL, 5 )
+		gSizer51 = wx.GridSizer( 0, 2, 0, 0 )
 		
-		self.m_staticText5 = wx.StaticText( self, wx.ID_ANY, u"MyLabel", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.m_staticText5.Wrap( -1 )
-		gSizer5.Add( self.m_staticText5, 0, wx.ALL, 5 )
+		self.m_staticText171 = wx.StaticText( self, wx.ID_ANY, u"Parameter", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_staticText171.Wrap( -1 )
+		gSizer51.Add( self.m_staticText171, 0, wx.ALL, 5 )
 		
-		self.m_staticText6 = wx.StaticText( self, wx.ID_ANY, u"MyLabel", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.m_staticText6.Wrap( -1 )
-		gSizer5.Add( self.m_staticText6, 0, wx.ALL, 5 )
+		self.m_spinCtrl1 = wx.SpinCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, wx.SP_ARROW_KEYS, 0, 10, 0 )
+		gSizer51.Add( self.m_spinCtrl1, 0, wx.ALL|wx.EXPAND, 5 )
 		
-		self.m_staticText7 = wx.StaticText( self, wx.ID_ANY, u"MyLabel", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.m_staticText7.Wrap( -1 )
-		gSizer5.Add( self.m_staticText7, 0, wx.ALL, 5 )
 		
-		self.m_staticText8 = wx.StaticText( self, wx.ID_ANY, u"MyLabel", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.m_staticText8.Wrap( -1 )
-		gSizer5.Add( self.m_staticText8, 0, wx.ALL, 5 )
+		gSizer5.Add( gSizer51, 1, wx.EXPAND, 5 )
 		
-		self.m_staticText9 = wx.StaticText( self, wx.ID_ANY, u"MyLabel", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.m_staticText9.Wrap( -1 )
-		gSizer5.Add( self.m_staticText9, 0, wx.ALL, 5 )
+		gSizer511 = wx.GridSizer( 0, 2, 0, 0 )
 		
-		self.m_staticText10 = wx.StaticText( self, wx.ID_ANY, u"MyLabel", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.m_staticText10.Wrap( -1 )
-		gSizer5.Add( self.m_staticText10, 0, wx.ALL, 5 )
+		self.m_staticText1711 = wx.StaticText( self, wx.ID_ANY, u"Parameter", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_staticText1711.Wrap( -1 )
+		gSizer511.Add( self.m_staticText1711, 0, wx.ALL, 5 )
 		
-		self.m_staticText11 = wx.StaticText( self, wx.ID_ANY, u"MyLabel", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.m_staticText11.Wrap( -1 )
-		gSizer5.Add( self.m_staticText11, 0, wx.ALL, 5 )
+		self.m_spinCtrl11 = wx.SpinCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, wx.SP_ARROW_KEYS, 0, 10, 0 )
+		gSizer511.Add( self.m_spinCtrl11, 0, wx.ALL|wx.EXPAND, 5 )
 		
-		self.m_staticText12 = wx.StaticText( self, wx.ID_ANY, u"MyLabel", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.m_staticText12.Wrap( -1 )
-		gSizer5.Add( self.m_staticText12, 0, wx.ALL, 5 )
 		
-		self.m_staticText13 = wx.StaticText( self, wx.ID_ANY, u"MyLabel", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.m_staticText13.Wrap( -1 )
-		gSizer5.Add( self.m_staticText13, 0, wx.ALL, 5 )
+		gSizer5.Add( gSizer511, 1, wx.EXPAND, 5 )
 		
-		self.m_staticText14 = wx.StaticText( self, wx.ID_ANY, u"MyLabel", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.m_staticText14.Wrap( -1 )
-		gSizer5.Add( self.m_staticText14, 0, wx.ALL, 5 )
+		gSizer512 = wx.GridSizer( 0, 2, 0, 0 )
 		
-		self.m_staticText15 = wx.StaticText( self, wx.ID_ANY, u"MyLabel", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.m_staticText15.Wrap( -1 )
-		gSizer5.Add( self.m_staticText15, 0, wx.ALL, 5 )
+		self.m_staticText1712 = wx.StaticText( self, wx.ID_ANY, u"Parameter", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_staticText1712.Wrap( -1 )
+		gSizer512.Add( self.m_staticText1712, 0, wx.ALL, 5 )
 		
-		self.m_staticText16 = wx.StaticText( self, wx.ID_ANY, u"MyLabel", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.m_staticText16.Wrap( -1 )
-		gSizer5.Add( self.m_staticText16, 0, wx.ALL, 5 )
+		self.m_spinCtrl12 = wx.SpinCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, wx.SP_ARROW_KEYS, 0, 10, 0 )
+		gSizer512.Add( self.m_spinCtrl12, 0, wx.ALL|wx.EXPAND, 5 )
 		
-		self.m_staticText17 = wx.StaticText( self, wx.ID_ANY, u"MyLabel", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.m_staticText17.Wrap( -1 )
-		gSizer5.Add( self.m_staticText17, 0, wx.ALL, 5 )
 		
-		self.m_staticText18 = wx.StaticText( self, wx.ID_ANY, u"MyLabel", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.m_staticText18.Wrap( -1 )
-		gSizer5.Add( self.m_staticText18, 0, wx.ALL, 5 )
+		gSizer5.Add( gSizer512, 1, wx.EXPAND, 5 )
 		
-		self.m_staticText19 = wx.StaticText( self, wx.ID_ANY, u"MyLabel", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.m_staticText19.Wrap( -1 )
-		gSizer5.Add( self.m_staticText19, 0, wx.ALL, 5 )
+		gSizer513 = wx.GridSizer( 0, 2, 0, 0 )
+		
+		self.m_staticText1713 = wx.StaticText( self, wx.ID_ANY, u"Parameter", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_staticText1713.Wrap( -1 )
+		gSizer513.Add( self.m_staticText1713, 0, wx.ALL, 5 )
+		
+		self.m_spinCtrl13 = wx.SpinCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, wx.SP_ARROW_KEYS, 0, 10, 0 )
+		gSizer513.Add( self.m_spinCtrl13, 0, wx.ALL|wx.EXPAND, 5 )
+		
+		
+		gSizer5.Add( gSizer513, 1, wx.EXPAND, 5 )
+		
+		gSizer514 = wx.GridSizer( 0, 2, 0, 0 )
+		
+		self.m_staticText1714 = wx.StaticText( self, wx.ID_ANY, u"Parameter", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_staticText1714.Wrap( -1 )
+		gSizer514.Add( self.m_staticText1714, 0, wx.ALL, 5 )
+		
+		self.m_spinCtrl14 = wx.SpinCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, wx.SP_ARROW_KEYS, 0, 10, 0 )
+		gSizer514.Add( self.m_spinCtrl14, 0, wx.ALL|wx.EXPAND, 5 )
+		
+		
+		gSizer5.Add( gSizer514, 1, wx.EXPAND, 5 )
+		
+		gSizer515 = wx.GridSizer( 0, 2, 0, 0 )
+		
+		self.m_staticText1715 = wx.StaticText( self, wx.ID_ANY, u"Parameter", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_staticText1715.Wrap( -1 )
+		gSizer515.Add( self.m_staticText1715, 0, wx.ALL, 5 )
+		
+		self.m_spinCtrl15 = wx.SpinCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, wx.SP_ARROW_KEYS, 0, 10, 0 )
+		gSizer515.Add( self.m_spinCtrl15, 0, wx.ALL|wx.EXPAND, 5 )
+		
+		
+		gSizer5.Add( gSizer515, 1, wx.EXPAND, 5 )
+		
+		gSizer516 = wx.GridSizer( 0, 2, 0, 0 )
+		
+		self.m_staticText1716 = wx.StaticText( self, wx.ID_ANY, u"Parameter", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_staticText1716.Wrap( -1 )
+		gSizer516.Add( self.m_staticText1716, 0, wx.ALL, 5 )
+		
+		self.m_spinCtrl16 = wx.SpinCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, wx.SP_ARROW_KEYS, 0, 10, 0 )
+		gSizer516.Add( self.m_spinCtrl16, 0, wx.ALL|wx.EXPAND, 5 )
+		
+		
+		gSizer5.Add( gSizer516, 1, wx.EXPAND, 5 )
+		
+		gSizer517 = wx.GridSizer( 0, 2, 0, 0 )
+		
+		self.m_staticText1717 = wx.StaticText( self, wx.ID_ANY, u"Parameter", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_staticText1717.Wrap( -1 )
+		gSizer517.Add( self.m_staticText1717, 0, wx.ALL, 5 )
+		
+		self.m_spinCtrl17 = wx.SpinCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, wx.SP_ARROW_KEYS, 0, 10, 0 )
+		gSizer517.Add( self.m_spinCtrl17, 0, wx.ALL|wx.EXPAND, 5 )
+		
+		
+		gSizer5.Add( gSizer517, 1, wx.EXPAND, 5 )
+		
+		gSizer518 = wx.GridSizer( 0, 2, 0, 0 )
+		
+		self.m_staticText1718 = wx.StaticText( self, wx.ID_ANY, u"Parameter", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_staticText1718.Wrap( -1 )
+		gSizer518.Add( self.m_staticText1718, 0, wx.ALL, 5 )
+		
+		self.m_spinCtrl18 = wx.SpinCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, wx.SP_ARROW_KEYS, 0, 10, 0 )
+		gSizer518.Add( self.m_spinCtrl18, 0, wx.ALL|wx.EXPAND, 5 )
+		
+		
+		gSizer5.Add( gSizer518, 1, wx.EXPAND, 5 )
+		
+		gSizer519 = wx.GridSizer( 0, 2, 0, 0 )
+		
+		self.m_staticText1719 = wx.StaticText( self, wx.ID_ANY, u"Parameter", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_staticText1719.Wrap( -1 )
+		gSizer519.Add( self.m_staticText1719, 0, wx.ALL, 5 )
+		
+		self.m_spinCtrl19 = wx.SpinCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, wx.SP_ARROW_KEYS, 0, 10, 0 )
+		gSizer519.Add( self.m_spinCtrl19, 0, wx.ALL|wx.EXPAND, 5 )
+		
+		
+		gSizer5.Add( gSizer519, 1, wx.EXPAND, 5 )
+		
+		self.Btn_LoadParams = wx.Button( self, wx.ID_ANY, u"Load onto Device", wx.DefaultPosition, wx.DefaultSize, 0 )
+		gSizer5.Add( self.Btn_LoadParams, 0, wx.ALL|wx.EXPAND, 5 )
 		
 		
 		gSizer1.Add( gSizer5, 1, wx.EXPAND, 5 )
