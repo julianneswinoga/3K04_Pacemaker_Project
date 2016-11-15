f88226110748df0196005a6768e1adc1a3e3e93f
laruepa <laruepa@mcmaster.ca>
2016-11-14 20:41:35 -0500

    Namespace bs added to Sense class to accomidate enum types with same value e.g. "DUAL". Compiles ok.

--- src/Sense.h
+++ src/Sense.h
@@ -12,15 +12,18 @@ namespace activityresponse{
 	typedef enum {NONE,TRIGGERED,INHIBITED,DUAL} ACTIVITYRESPONSE;
 }
 
+typedef enum {V_LOW,LOW,MED_LOW,MED,MED_HIGH,HIGH,V_HIGH} ACTIVITYTHRESHOLD;
+
 class Sense : public Pacemaker {
 	private:
 		chambers::CHAMBERS chambersSensed;
 		activityresponse::ACTIVITYRESPONSE activityResponse;
 		bool magnetInPlace;
-		//enum activityThreshold;
+		ACTIVITYTHRESHOLD activityThreshold;
 	protected:
 		void setChambersSensed(chambers::CHAMBERS);
-		//void setActivityResponse(enum);
+		void setActivityResponse(activityresponse::ACTIVITYRESPONSE);
+		void setActivityThreshold(ACTIVITYTHRESHOLD);
 		uint16_t maxSensorRate;
 		void setMagnetInPlace(bool);
 		void measureLeadImpedance();
@@ -30,6 +33,7 @@ class Sense : public Pacemaker {
 		Sense();
 		chambers::CHAMBERS getChambersSensed();
 		activityresponse::ACTIVITYRESPONSE getActivityResponse();
+		ACTIVITYTHRESHOLD getActivityThreshold();
 		bool getMagnetInPlace();
 };
 
