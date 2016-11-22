bbf610b59cab28a93e3ad4e2daceabd144af6201
Cameron Swinoga <swinogca@mcmaster.ca>
2016-11-22 12:51:48 -0500

    Added and modified accelerometer driver

--- src/Activity.h
+++ src/Activity.h
@@ -2,12 +2,14 @@
 #define ACTIVITY_H
 
 #include "mbed.h"
+#include "FXOS8700Q.h"
 
 enum class ACTIVITYSTATE { V_LOW, LOW, MED_LOW, MED, MED_HIGH, HIGH, V_HIGH };
 
 class Activity {
 	private:
 		uint16_t activityTimeThresholdSeconds = 5;
+		FXOS8700Q_acc accelerometer;
 		
 	public:
 		Activity();
