0661f3e46126ed59eefa16a2edcc3fb704f9fe96
Cameron Swinoga <swinogca@mcmaster.ca>
2016-12-11 15:11:57 -0500

    Simulated data working

--- gui/pacemakerInterfaceMainFrame.py
+++ gui/pacemakerInterfaceMainFrame.py
@@ -2,19 +2,18 @@ import wx
 import pacemakerInterface
 import wx.lib.plot as plot
 
-import numpy, struct, binascii, threading
+import numpy, struct, binascii, threading, time, csv
 
 from serialHelper import *
 
 def rotate(list, n):
-		return list[-n:] + list[:-n]
+	return list[-n:] + list[:-n]
 		
 def scale_bitmap(bitmap, width, height):
 	image = bitmap.ConvertToImage()
 	image = image.Scale(width, height, wx.IMAGE_QUALITY_HIGH)
 	result = wx.Bitmap(image)
 	return result
-
 
 def threaded(fn):
 	def wrapper(*args, **kwargs):
@@ -45,6 +44,14 @@ class pacemakerInterfaceMainFrame(pacemakerInterface.MainFrame):
 		self.runSerialThread = True
 		self.serialThreadExit = False
 		
+		self.simulating = False
+		self.simulatedData = []
+		self.simulatedDataPosition = 0
+		with open('simulatedECG.csv', 'rb') as csvfile:
+			csvReader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+			for row in csvReader:
+				self.simulatedData.append(float(row[0]))
+		
 		self.StaticBitmapDisconnected = scale_bitmap(wx.Bitmap('img/disconnected.png', wx.BITMAP_TYPE_ANY), 50, 50)
 		self.StaticBitmapConnected = scale_bitmap(wx.Bitmap('img/connected.png', wx.BITMAP_TYPE_ANY), 50, 50)
 		self.Img_Connected.SetBitmap(self.StaticBitmapDisconnected)
@@ -56,32 +63,41 @@ class pacemakerInterfaceMainFrame(pacemakerInterface.MainFrame):
 	@threaded
 	def serialFunction(self):
 		while (self.runSerialThread):
-			if (self.SerialInterface != None and self.SerialInterface.is_open and self.SerialInterface.in_waiting >= 0):				
-				buf = ''
-				try:
-					data = self.SerialInterface.read()
-				except:
-					continue
-				while (data != "\n" and self.runSerialThread):
-					buf += data
+			if (self.simulating):
+				self.pointsToAdd.append(self.simulatedData[self.simulatedDataPosition])
+				if (self.simulatedDataPosition < len(self.simulatedData) - 1):
+					self.simulatedDataPosition += 1
+				else:
+					self.simulatedDataPosition = 0
+				for i in range(100000): # Wait loop
+					pass
+			else:
+				if (self.SerialInterface != None and self.SerialInterface.is_open and self.SerialInterface.in_waiting >= 0):				
+					buf = ''
 					try:
 						data = self.SerialInterface.read()
 					except:
-						continue
-				
-				try:
-					point = struct.unpack('f', buf)[0]
-					self.pointsToAdd.append(point)
-				except:
-					print buf
+						pass
+					while (data != "\n" and self.runSerialThread):
+						buf += data
+						try:
+							data = self.SerialInterface.read()
+						except:
+							pass
+					
+					try:
+						point = struct.unpack('f', buf)[0]
+						self.pointsToAdd.append(point)
+					except:
+						print buf
 		self.serialThreadExit = True
 	
 	def OnGraphUpdateTimer(self, event):
-		if (self.SerialInterface != None and self.SerialInterface.is_open and self.SerialInterface.in_waiting >= 1000):
-			print 'Overload:', self.SerialInterface.in_waiting
+		if (self.SerialInterface != None and self.SerialInterface.is_open):
+			self.gauge_bufferSize.SetValue(self.SerialInterface.in_waiting)
 	
 		while(len(self.pointsToAdd) > 0):
-			self.AddPoint(self.pointsToAdd.pop())
+			self.AddPoint(self.pointsToAdd.pop(0))
 		self.UpdateGraph()
 	
 	def AddPoint(self, y):
@@ -92,6 +108,7 @@ class pacemakerInterfaceMainFrame(pacemakerInterface.MainFrame):
 		pts = [[c, self.plotPoints[c]] for c in range(len(self.plotPoints))]
 		line = plot.PolyLine(pts, colour='green', legend='2', width=1)
 		self.canvas.Draw(plot.PlotGraphics([line]))
+		print dir(plot)
 		
 	def OnScanBttnClicked(self, event):
 		for i in self.serialPortsAvailable:
@@ -105,6 +122,13 @@ class pacemakerInterfaceMainFrame(pacemakerInterface.MainFrame):
 	
 	def OnBttnStartStopStreamClicked(self, event):
 		if (self.SerialInterface == None or not self.SerialInterface.is_open):
+			if (self.checkBox_simulateData.GetValue()):
+				if (self.simulating):
+					self.Bttn_StartStopStream.SetLabel('Start Streaming')
+					self.simulating = False
+				else:
+					self.Bttn_StartStopStream.SetLabel('Stop Streaming')
+					self.simulating = True
 			return
 		if (self.streamingData):
 			self.SerialInterface.write(struct.pack('B', 3))
@@ -118,38 +142,48 @@ class pacemakerInterfaceMainFrame(pacemakerInterface.MainFrame):
 	
 	def OnConnectBttnClicked(self, event):
 		if (self.SerialInterface == None or not self.SerialInterface.is_open): # Connect to a device
-			activeText = self.SerialDropdown.GetStringSelection()
-			if (activeText == ''):
-				return
-			print 'Connecting to ' + activeText + '\n'
-			
-			self.SerialInterface = open_port(activeText, baud = 57600, timeout = 3)
-			if (self.SerialInterface.is_open):
-				self.Bttn_ConnectDisconnect.SetLabel('Disconnect')
-				self.Img_Connected.SetBitmap(self.StaticBitmapConnected)
-				
-				self.SerialInterface.write(struct.pack('B', 4)) # send connect signal
-				self.SerialInterface.flush()
-				
-				self.SerialInterface.write(struct.pack('B', 0))
-				self.SerialInterface.write('123456789\nbbbbbbbbbbb\ncccccc\n')
-				self.SerialInterface.flush()
-				
-				
-			else:
-				print 'Serial connection to ' + activeText + ' failed!'
+			self.connectSerial()
 		elif (self.SerialInterface != None and self.SerialInterface.is_open): # Disconnect from a device
-			self.SerialInterface.write(struct.pack('B', 5)) # send disconnect signal
-			self.SerialInterface.flush()
+			self.disconnectSerial()
+			
+	def connectSerial(self):
+		activeText = self.SerialDropdown.GetStringSelection()
+		if (activeText == ''):
+			return
+		print 'Connecting to ' + activeText + '\n'
 		
-			self.SerialInterface.close()
-			if (not self.SerialInterface.is_open):
-				self.Bttn_ConnectDisconnect.SetLabel('Connect')
-				self.Img_Connected.SetBitmap(self.StaticBitmapDisconnected)
-				self.streamingData = False
-				self.Bttn_StartStopStream.SetLabel('Start Streaming')
-			else:
-				print 'Failed to disconnect from serial device!'
+		self.SerialInterface = open_port(activeText, baud = 57600, timeout = 3)
+		if (self.SerialInterface.is_open):
+			self.Bttn_ConnectDisconnect.SetLabel('Disconnect')
+			self.Img_Connected.SetBitmap(self.StaticBitmapConnected)
+			
+			self.SerialInterface.write(struct.pack('B', 4)) # send connect signal
+			self.SerialInterface.flush()
+			
+			self.SerialInterface.write(struct.pack('B', 0))
+			self.SerialInterface.write('123456789\nbbbbbbbbbbb\ncccccc\n')
+			self.SerialInterface.flush()
+			
+		else:
+			print 'Serial connection to ' + activeText + ' failed!'
+	
+	def disconnectSerial(self):
+		self.SerialInterface.write(struct.pack('B', 5)) # send disconnect signal
+		self.SerialInterface.flush()
+	
+		self.SerialInterface.close()
+		if (not self.SerialInterface.is_open):
+			self.Bttn_ConnectDisconnect.SetLabel('Connect')
+			self.Img_Connected.SetBitmap(self.StaticBitmapDisconnected)
+			self.streamingData = False
+			self.Bttn_StartStopStream.SetLabel('Start Streaming')
+		else:
+			print 'Failed to disconnect from serial device!'
+	
+	def OnSimulateDataChecked(self, event):
+		if (self.checkBox_simulateData.GetValue()):
+			if (self.SerialInterface != None and self.SerialInterface.is_open): # If we're connected, we need to disconnect
+				self.disconnectSerial()
 				
 	def OnLoadBttnClicked(self, event):
 		if (self.SerialInterface == None or not self.SerialInterface.is_open):
